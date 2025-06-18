dd0dd7b58a16899f25c2fc07b7f47da3e6bbc466
DarkwingDelphi <109265941+DarkwingDelphi@users.noreply.github.com>
2025-06-17 22:30:43 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
 let gameState = "start";
 let playerImage, doorImage, fartSound;
-let gravity = 0.2;
-let jumpForce = -5;
+let gravity = 0.25;
+let jumpForce = -7;
 let score = 0;
 let startTime;
 let finger;
-
-let canvasW = 240;
-let canvasH = 480;
+let canvasW, canvasH;
 
 function preload() {
   playerImage = loadImage("finger.png");
@@ -17,11 +15,11 @@ function preload() {
 }
 
 function setup() {
-  let cnv = createCanvas(canvasW, canvasH);
-  cnv.parent(document.body);
+  canvasW = windowWidth;
+  canvasH = windowHeight;
+  createCanvas(canvasW, canvasH);
   finger = new Finger();
   textAlign(CENTER, CENTER);
-  textSize(20);
 }
 
 function draw() {
@@ -29,21 +27,25 @@ function draw() {
 
   if (gameState === "start") {
     fill(0);
-    text("DON’T TOUCH THE DOOR", width / 2, height / 2 - 20);
-    textSize(14);
+    textSize(32);
+    text("DON’T TOUCH THE DOOR", width / 2, height / 2 - 30);
+    textSize(18);
     text("Tap to start", width / 2, height / 2 + 10);
   }
 
   else if (gameState === "play") {
+    let doorW = width * 0.35;
+    let doorH = doorW * 1.5;
     imageMode(CENTER);
-    image(doorImage, width / 2, height - 150, 200, 300);
+    image(doorImage, width / 2, height - doorH / 2, doorW, doorH);
     finger.update();
     finger.show();
 
+    // Collision
     if (
-      finger.y + finger.h / 2 > height - 150 &&
-      finger.x > width / 2 - 100 &&
-      finger.x < width / 2 + 100
+      finger.y + finger.h / 2 > height - doorH &&
+      finger.x > width / 2 - doorW / 2 &&
+      finger.x < width / 2 + doorW / 2
     ) {
       if (fartSound.isLoaded()) fartSound.play();
       gameState = "end";
@@ -51,17 +53,17 @@ function draw() {
 
     score = floor((millis() - startTime) / 100);
     fill(0);
-    textSize(14);
-    text("Score: " + score, width / 2, 20);
+    textSize(18);
+    text("Score: " + score, width / 2, 30);
   }
 
   else if (gameState === "end") {
     fill(0);
-    textSize(20);
+    textSize(28);
     text("YOU TOUCHED THE DOOR", width / 2, height / 2 - 20);
-    textSize(16);
-    text("Final Score: " + score, width / 2, height / 2 + 10);
-    text("Tap to restart", width / 2, height / 2 + 40);
+    textSize(20);
+    text("Final Score: " + score, width / 2, height / 2 + 20);
+    text("Tap to restart", width / 2, height / 2 + 60);
   }
 }
 
@@ -81,8 +83,8 @@ function touchStarted() {
 
 class Finger {
   constructor() {
-    this.w = 240;
-    this.h = 360;
+    this.w = width * 0.45;
+    this.h = this.w * 1.5;
     this.reset();
   }
 
@@ -99,7 +101,6 @@ class Finger {
   update() {
     this.vy += gravity;
     this.y += this.vy;
-
     if (this.y > height - this.h / 2) {
       this.y = height - this.h / 2;
       this.vy = 0;
@@ -110,4 +111,8 @@ class Finger {
     imageMode(CENTER);
     image(playerImage, this.x, this.y, this.w, this.h);
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
